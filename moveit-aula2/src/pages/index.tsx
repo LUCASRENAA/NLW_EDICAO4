b0a8535e9cc24c4503caba63be78c8e62bd2c45e
Lucas Meira <lrms2@discente.ifpe.edu.br>
2021-02-24 05:40:32 -0300

    aula2

--- moveit-aula2/src/pages/index.tsx
+++ moveit-aula2/src/pages/index.tsx
@@ -1,15 +1,32 @@
+import { CompletedChallenges } from '../components/CompletedChallenges'
+import { Countdown } from '../components/Countdown'
+import { ExperienceBar } from '../components/ExperienceBar'
+import { Profile } from '../components/Profile'
+
 import Head from 'next/head'
-import { ExperienceBar } from "../components/ExperienceBar";
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div  className="container">
-      <Head>
-      <link rel="preconnect" href="https://fonts.gstatic.com"/>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
     
-      </Head>
-      <ExperienceBar />
+    <div className={styles.container}>
+  
+
+  <ExperienceBar />
+
+      <section>
+        <div className={styles.container}>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div className={styles.container}>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+      </section>
     </div>
-    )
+  )
 }
